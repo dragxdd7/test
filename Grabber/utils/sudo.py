f570d02603fc695c977038b74213610c48316874
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-07-17 11:33:42 +0530

    Update utils

---
+++ Grabber/utils/sudo.py
@@ -0,0 +1,13 @@
+from Grabber import db
+
+sudb = db.sudo
+
+async def get_sudo_user_ids():
+    sudo_users = await sudb.find({}, {'user_id': 1}).to_list(length=None)
+    return [user['user_id'] for user in sudo_users]
+
+async def is_sudo_user(_, __, message: Message):
+    sudo_user_ids = await get_sudo_user_ids()
+    return message.from_user.id in sudo_user_ids
+
+sudo_filter = filters.create(is_sudo_user)
