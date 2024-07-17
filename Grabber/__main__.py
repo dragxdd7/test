448dd0a24824255a59b7875599d2cd5f4b3ca578
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-07-17 11:15:05 +0530

    Update

---
+++ Grabber/__main__.py
@@ -0,0 +1,35 @@
+import importlib
+import time
+import re
+import asyncio
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters
+
+from Grabber import collection, Grabberu, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection
+from Grabber import application, LOGGER
+from Grabber.modules import ALL_MODULES
+
+locks = {}
+message_counters = {}
+spam_counters = {}
+last_user = {}
+warned_users = {}
+message_counts = {}
+
+for module_name in ALL_MODULES:
+    imported_module = importlib.import_module("Grabber.modules." + module_name)
+
+def escape_markdown(text):
+    escape_chars = r'\*_`\\~>#+-=|{}.!'
+    return re.sub(r'([%s])' % re.escape(escape_chars), r'\\\1', text)
+
+
+def main() -> None:
+    """Run bot."""
+
+    application.run_polling(drop_pending_updates=True)
+
+if __name__ == '__main__':
+    Grabberu.start()
+    LOGGER.info("Bot started")
+    main()
