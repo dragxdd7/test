ecc00718f035877e56bf40473a6d7dde8ee671d8
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-07-17 19:34:56 +0530

    Update battle.py

--- Grabber/modules/battle.py
+++ Grabber/modules/battle.py
@@ -35,6 +35,44 @@ def format_timedelta(delta):
     else:
         return f"{seconds}s"
 
+@Grabberu.on_message(filters.command("battle") & filters.reply)
+async def battle_command(client, message):
+    user_a_id = message.from_user.id
+    user_a_data = await user_collection.find_one({'id': user_a_id})
+
+    if not user_a_data or ('clan_id' not in user_a_data and 'leader_id' not in user_a_data):
+        await message.reply_text("You need to be part of a clan or a clan leader to use this command.")
+        return
+
+    user_b_id = message.reply_to_message.from_user.id
+    user_b_data = await user_collection.find_one_and_update(
+        {'id': user_b_id},
+        {'$setOnInsert': {'id': user_b_id, 'first_name': message.reply_to_message.from_user.first_name, 'gold': 0, 'weapons': []}},
+        upsert=True,
+        return_document=True
+    )
+
+    if not user_b_data:
+        await message.reply_text("Opponent information not found and could not be created.")
+        return
+
+    user_a_name = user_a_data.get('first_name', 'User A')
+    user_b_name = user_b_data.get('first_name', 'User B')
+
+    # Log attacker's weapons
+    attacker_weapons = user_a_data.get('weapons', [])
+
+    # Log defender's weapons
+    defender_weapons = user_b_data.get('weapons', [])
+
+    keyboard = [
+        [InlineKeyboardButton("Fight", callback_data=f"battle_accept:{user_a_id}:{user_b_id}"),
+         InlineKeyboardButton("Run", callback_data=f"battle_decline:{user_a_id}:{user_b_id}")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await message.reply_text(f"{user_b_name}, {user_a_name} challenged you: Do you fight or run?", reply_markup=reply_markup)
+
 @Grabberu.on_callback_query(filters.regex(r'^battle_accept'))
 async def handle_battle_accept(client, query: CallbackQuery):
     data = query.data.split(':')
