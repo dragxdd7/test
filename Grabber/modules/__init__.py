3f27385cb58b98215298ea76eeefcee4ddc62225
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-07-17 13:34:52 +0530

    Update __init__.py

--- Grabber/modules/__init__.py
+++ Grabber/modules/__init__.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 import time
-from Grabber import user_collection , application, db, collection, clan_collection , Grabberu 
+from Grabber import user_collection , application, db, collection, clan_collection , Grabberu ,clan_collection, join_requests_collection
 from functools import wraps
 from telegram import Update
 from Grabber.utils import * 
