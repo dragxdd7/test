b3015120eb6fb5d30c98aaa0ca1a0011f139b392
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-07-18 10:09:58 +0530

    Update delta.py

--- Grabber/modules/delta.py
+++ Grabber/modules/delta.py
@@ -1,56 +1,81 @@
+from telegram import Update, InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
+from telegram.ext import CommandHandler, CallbackContext, MessageHandler, Filters
+from PIL import Image, ImageDraw, ImageFont
 import random
 import io
-from PIL import Image, ImageDraw, ImageFont
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM, CallbackQuery
-from . import add, deduct, show, abank, dbank, sbank, app, user_collection, collection, sudo_filter
+import time
+from words import words
+from Grabber import application, user_collection
+from . import add, deduct, show, application ,sudb
 
 BG_IMAGE_PATH = "Images/blue.jpg"
-DEFAULT_MESSAGE_LIMIT = 30
+DEFAULT_MESSAGE_LIMIT = 45
 group_message_counts = {}
-math_questions = {}
+alpha_dict = {}
+guess_start_time = {}
 
-def generate_random_math_equation_image():
-    num1 = random.randint(100, 999)
-    num2 = random.randint(100, 999)
-    operation = random.choice(['+', '-', '*'])
-    if operation == '+':
-        answer = num1 + num2
-    elif operation == '-':
-        answer = num1 - num2
-    else:
-        answer = num1 * num2
-    equation = f"{num1} {operation} {num2} = ?"
-
+def generate_random_image(word: str) -> bytes:
     img = Image.open(BG_IMAGE_PATH)
     d = ImageDraw.Draw(img)
-    fnt = ImageFont.truetype('Fonts/font.ttf', 60)
-    text_width, text_height = d.textsize(equation, font=fnt)
+    fnt = ImageFont.truetype('Fonts/font.ttf', 76)
+    text_width, text_height = d.textsize(word, font=fnt)
     text_x = (img.width - text_width) / 2
     text_y = (img.height - text_height) / 2
-    d.text((text_x, text_y), equation, font=fnt, fill=(0, 0, 0))
+    d.text((text_x, text_y), word, font=fnt, fill=(0, 0, 0))
 
     img_byte_arr = io.BytesIO()
     img.save(img_byte_arr, format='PNG')
     img_byte_arr.seek(0)
 
-    return img_byte_arr, answer 
+    return img_byte_arr.read()
 
-@app.on_message(filters.command("stime") & sudo_filter)
-async def set_message_limit(client, message):
+async def get_sudo_user_ids():
+    sudo_users = await sudb.find({}, {'user_id': 1}).to_list(length=None)
+    return [user['user_id'] for user in sudo_users]
+
+async def set_message_limit(update: Update, context: CallbackContext):
     try:
-        limit = int(message.command[1])
-        if limit <= 0:
-            await message.reply_text("Message limit must be a positive integer!")
+        sudo_user_ids = await get_sudo_user_ids()
+        user_id = update.effective_user.id
+        if user_id not in sudo_user_ids:
+            await update.message.reply_text("Only sudo users can set the message limit!")
             return
 
-        group_message_counts[message.chat.id] = {'count': 0, 'limit': limit}
-        await message.reply_text(f"Message limit set to {limit}. Now spawning math equations every {limit} messages!")
-    except (IndexError, ValueError):
-        await message.reply_text("Please provide a valid message limit (integer).")
+        limit = int(context.args[0])
+        if limit <= 0:
+            await update.message.reply_text("Message limit must be a positive integer!")
+            return
 
-async def delta(client, query: CallbackQuery):
-    chat_id = query.message.chat.id  # Access chat_id through query.message.chat.id
+        group_message_counts[update.effective_chat.id] = {'count': 0, 'limit': limit}
+        await update.message.reply_text(f"Message limit set to {limit}. Now spawning images every {limit} messages!")
+    except (IndexError, ValueError):
+        await update.message.reply_text("Please provide a valid message limit (integer).")
+
+async def handle_guess(update: Update, context: CallbackContext):
+    if not update.effective_message.text:
+        return
+    message_text = update.effective_message.text.strip().lower()
+    chat_id = update.effective_chat.id
+    user_id = update.effective_user.id
+    user_name = update.effective_user.first_name
+
+    if chat_id in alpha_dict:
+        correct_word = alpha_dict[chat_id]
+
+        if message_text == correct_word:
+            reward_amount = random.randint(20000, 40000)
+            alpha_dict.pop(chat_id)
+
+            # Calculate the time taken
+            end_time = time.time()
+            time_taken = int(end_time - guess_start_time.pop(chat_id))
+
+            await update.message.reply_text(f"{user_name}, you got {reward_amount} 🔖 for completing the word!\n\nTime taken: {time_taken}s")
+            await add(user_id, reward_amount)
+
+async def handle_messages(update: Update, context: CallbackContext):
+    await handle_guess(update, context)
+    chat_id = update.effective_chat.id
 
     if chat_id in group_message_counts:
         group_message_counts[chat_id]['count'] += 1
@@ -60,40 +85,17 @@ async def delta(client, query: CallbackQuery):
     if group_message_counts[chat_id]['limit'] and group_message_counts[chat_id]['count'] >= group_message_counts[chat_id]['limit']:
         group_message_counts[chat_id]['count'] = 0
 
-        image_bytes, answer = generate_random_math_equation_image()
-        math_questions[chat_id] = answer
+        random_word = random.choice(words)
+        image_bytes = generate_random_image(random_word)
+
+        alpha_dict[chat_id] = random_word
+        guess_start_time[chat_id] = time.time()  # Start timing
 
         keyboard = [
-            [IKB(str(answer), callback_data='correct')],
-            [IKB(str(random.randint(100, 999)), callback_data='incorrect1')],
-            [IKB(str(random.randint(100, 999)), callback_data='incorrect2')],
-            [IKB(str(random.randint(100, 999)), callback_data='incorrect3')]
+            [IKB("Join", url="https://t.me/dragons_support")]
         ]
-        random.shuffle(keyboard)
+        reply_markup = IKM(keyboard)
 
-        # Correcting the structure of the keyboard for 2x2 layout
-        reply_markup = IKM([[keyboard[0][0], keyboard[1][0]], [keyboard[2][0], keyboard[3][0]]])
+        await context.bot.send_photo(chat_id=chat_id, photo=image_bytes, caption=f"Guess the word in the image to win!", reply_markup=reply_markup)
 
-        await client.send_photo(chat_id=chat_id, photo=image_bytes, caption="Solve the math equation!", reply_markup=reply_markup)
-
-async def sumu(client, query: CallbackQuery):
-    user_id = query.from_user.id
-    user_name = query.from_user.first_name
-    chat_id = query.message.chat.id
-
-    if chat_id in math_questions:
-        if query.data == 'correct':
-            reward_amount = random.randint(20000, 40000)
-            await query.answer(f"Correct! You earned {reward_amount} 🔖", show_alert=True)
-
-            await add(user_id, reward_amount)
-            new_caption = f"Solved by {user_name}! Congratulations! You earned {reward_amount} 🔖"
-            await query.message.edit_caption(caption=new_caption, reply_markup=None)
-
-            del math_questions[chat_id]
-        else:
-            await query.answer("Incorrect! Try again later.", show_alert=True)
-            await query.answer()
-
-
-app.on_callback_query()(sumu)
+application.add_handler(CommandHandler('wtime', set_message_limit))
