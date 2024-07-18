896ed8045b76e464e2e920b30c9896de177397d2
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-07-18 15:27:14 +0530

    Update sudo.py

--- Grabber/utils/sudo.py
+++ Grabber/utils/sudo.py
@@ -13,3 +13,15 @@ async def is_sudo_user(_, __, message: Message):
     return message.from_user.id in sudo_user_ids
 
 sudo_filter = filters.create(is_sudo_user)
+
+devdb = db.dev
+
+async def get_dev_user_ids():
+    dev_users = await devdb.find({}, {'user_id': 1}).to_list(length=None)
+    return [user['user_id'] for user in dev_users]
+
+async def is_dev_user(_, __, message: Message):
+    dev_user_ids = await get_dev_user_ids()
+    return message.from_user.id in dev_user_ids
+
+dev_filter = filters.create(is_dev_user)
