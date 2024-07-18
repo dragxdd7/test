971e7fed0e5aea1792385bb022b2ce715ee254fa
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-07-18 16:00:35 +0530

    Update _callbacka.py

--- Grabber/modules/_callbacka.py
+++ Grabber/modules/_callbacka.py
@@ -5,6 +5,7 @@ from .cmode import cmode_callback
 from Grabber.utils.button import button_click as bc
 from .delta import sumu
 from .harem import harem_callback as hc
+from .info import check
 
 async def cbq(update: Update, context: CallbackContext):
     query = update.callback_query
@@ -18,6 +19,8 @@ async def cbq(update: Update, context: CallbackContext):
         await sumu(update, context)
     elif data.startswith('harem'):
         await hc(update, context)
+    elif data.startswith('check_'):
+        await check(update, context)
 
     
 application.add_handler(CallbackQueryHandler(cbq, pattern='.*'))
