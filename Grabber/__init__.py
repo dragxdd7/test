5ffcc622827ad53da28961bd11f9933c4d786766
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-07-20 11:30:16 +0530

    Update __init__.py

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -1,16 +1,23 @@
-import logging  
+import logging
 
-from pyrogram import Client 
-
+from pyrogram import Client as PyrogramClient
 from telegram.ext import Application
 from motor.motor_asyncio import AsyncIOMotorClient
+from resolve_peer import ResolvePeer
+
+class Client(PyrogramClient):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def resolve_peer(self, id):
+        obj = ResolvePeer(self)
+        return await obj.resolve_peer(id)
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
     level=logging.INFO,
 )
-import logging
 
 logging.basicConfig(level=logging.DEBUG)
 logging.getLogger("apscheduler").setLevel(logging.ERROR)
@@ -21,9 +28,16 @@ LOGGER = logging.getLogger(__name__)
 OWNER_ID = "7185106962"
 GROUP_ID = "-1002235251549"
 TOKEN = "6942284208:AAGwdcnl1BMuEn7XlfI6dsk2kCWM1UWgJsY"
-#TOKEN = "6484632876:AAEoT5HShuVJUHT51d0KlGvO5Ev1GJT1MXg" #BETA 
 mongo_url = "mongodb+srv://ishitaroy657boobs:vUKC7qfTpj0oTbii@cluster0.ct6shax.mongodb.net/"
-PHOTO_URL =  ["https://graph.org/file/f10bec6ec695bba69037d.jpg","https://graph.org/file/bdac0dcb5d841263a8866.jpg","https://telegra.ph/file/75e758e6e47453d301fcd.jpg","https://telegra.ph/file/75e758e6e47453d301fcd.jpg","https://telegra.ph/file/dba87ad2a7d957588b8de.jpg","https://telegra.ph/file/765c9474d372cf40621da.jpg","https://telegra.ph/file/1e55c00d0be79b7ca614d.jpg"]
+PHOTO_URL = [
+    "https://graph.org/file/f10bec6ec695bba69037d.jpg",
+    "https://graph.org/file/bdac0dcb5d841263a8866.jpg",
+    "https://telegra.ph/file/75e758e6e47453d301fcd.jpg",
+    "https://telegra.ph/file/75e758e6e47453d301fcd.jpg",
+    "https://telegra.ph/file/dba87ad2a7d957588b8de.jpg",
+    "https://telegra.ph/file/765c9474d372cf40621da.jpg",
+    "https://telegra.ph/file/1e55c00d0be79b7ca614d.jpg"
+]
 SUPPORT_CHAT = "dragons_support"
 UPDATE_CHAT = "bot_support_arena"
 BOT_USERNAME = "Guess_Yourr_Waifu_bot"
@@ -44,6 +58,3 @@ guild = db["guild_team"]
 gban = db["gban"]
 clan_collection = db['clans']
 join_requests_collection = db['join_requests']
-clan_collection = db['clans']
-join_requests_collection = db['join_requests']
-
