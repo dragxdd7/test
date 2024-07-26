076d6a15e1a973a9cb5e599d34e63214f4cf9c6a
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-07-26 18:17:55 +0530

    Update utils

---
+++ Grabber/utils/character.py
@@ -0,0 +1,32 @@
+from pymongo import ReturnDocument
+
+async def ac(user_id: int, character_id: int):
+    try:
+        result = await user_collection.find_one_and_update(
+            {'id': user_id},
+            {'$push': {'characters': {'id': character_id}}},
+            upsert=True,
+            return_document=ReturnDocument.AFTER
+        )
+        if result:
+            return f"Character with ID {character_id} added to user {user_id}."
+        else:
+            return f"Failed to add character with ID {character_id} to user {user_id}."
+    except Exception as e:
+        print(f"Error in add_character: {e}")
+        return "An error occurred while adding the character."
+
+async def rc(user_id: int, character_id: int):
+    try:
+        result = await user_collection.find_one_and_update(
+            {'id': user_id, 'characters.id': character_id},
+            {'$pull': {'characters': {'id': character_id}}},
+            return_document=ReturnDocument.AFTER
+        )
+        if result:
+            return f"One instance of character with ID {character_id} removed from user {user_id}."
+        else:
+            return f"Character with ID {character_id} not found for user {user_id}."
+    except Exception as e:
+        print(f"Error in remove_character: {e}")
+        return "An error occurred while removing the character."
