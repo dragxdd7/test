875251b4a9e0ac962b423efd766c375934cd9a3c
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-08-02 11:15:50 +0530

    Update harem.py

--- Grabber/modules/harem.py
+++ Grabber/modules/harem.py
@@ -108,3 +108,5 @@ async def harem_callback(update: Update, context: CallbackContext) -> None:
         return
     
     await harem(update, context, page)
+
+application.add_handler(CommandHandler("harem", harem, block=False))
