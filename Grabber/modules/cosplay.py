d43b0e60fe889d0ed18c875e04c42f5d65d7b6f7
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-08-02 06:13:59 +0530

    Update modules

---
+++ Grabber/modules/cosplay.py
@@ -0,0 +1,20 @@
+import requests
+from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.enums import ChatAction
+from . import app
+
+@app.on_message(filters.command("cosplay"))
+async def cosplay(_, msg):
+    bot_info = await app.get_me()
+    bot_username = bot_info.username
+
+    DRAGONS = [
+        [
+            InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{bot_username}?startgroup=true"),
+        ],
+    ]
+
+    img = requests.get("https://waifu-api.vercel.app").json()
+    await msg.reply_photo(img, caption=f"❅ ᴄᴏsᴘʟᴀʏ ʙʏ ➠ ๛ᴅ ʀ ᴀ ɢ ᴏ ɴ ʏ ༗", reply_markup=InlineKeyboardMarkup(DRAGONS))
+
