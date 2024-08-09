0f1f35bb0507749e9e02cd811c7a704c810bf177
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-08-09 10:57:02 +0530

    Update __init__.py

--- Grabber/modules/__init__.py
+++ Grabber/modules/__init__.py
@@ -17,22 +17,23 @@ dev_users = {6919722801}
 ALPHABETS = "abcdefghijklmnopqrstuvwxyz"
 ALL_CAPS = "ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ"
 
-
-
 def capsify(text: str) -> str:
-  txt = ""
-  for x in text.split():
-    for y in x:
-      if y.lower() in ALPHABETS:
-        ind = ALPHABETS.index(y.lower())
-        txt += ALL_CAPS[ind]
-      else:
-        txt += y
-    txt += " "
-  return txt
+    txt = ""
+    for x in text:
+        if x == '\n':
+            txt += '\n'
+        elif x == ' ':
+            txt += ' '
+        else:
+            if x.lower() in ALPHABETS:
+                ind = ALPHABETS.index(x.lower())
+                txt += ALL_CAPS[ind]
+            else:
+                txt += x
+    return txt
 
 async def acapsify(text: str) -> str:
-  return capsify(text)
+    return capsify(text)
 
 
 logging.basicConfig(
