c6ef3ca314cabaafa991891258952b5488b9dcc3
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-08-23 16:27:48 +0530

    Update 1v1.py

--- Grabber/modules/1v1.py
+++ Grabber/modules/1v1.py
@@ -4,6 +4,10 @@ import random
 from . import user_collection, app
 import pyrogram.types as t
 
+
+async def get_user_data(user_id: int):
+    return await user_collection.find_one({'id': user_id})
+    
 # List of moves for each beast
 beast_moves = {
     1: ["Claw Swipe", "Pounce", "Roar", "Tail Whip"],
