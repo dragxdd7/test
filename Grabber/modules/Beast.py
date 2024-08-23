16e8403af585f65555f777a191cfc9d5bab05c10
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-08-23 14:59:03 +0530

    Update Beast.py

--- Grabber/modules/Beast.py
+++ Grabber/modules/Beast.py
@@ -16,7 +16,7 @@ beast_list = {
     7: {'name': '𝐂𝐚𝐥𝐚𝐭𝐡𝐢𝐞𝐥', 'price': 100000000, 'rarity': '🐉 𝐃𝐫𝐚𝐠𝐨𝐧', 'power': 200000, 'img_url': 'https://telegra.ph/file/0dbef2bf7ac6fc6f1c6c7.jpg'},
     8: {'name': '𝐆𝐞𝐧𝐞𝐯𝐚', 'price': 250000, 'rarity': '🍃 𝐆𝐨𝐛𝐥𝐢𝐧', 'power': 1000, 'img_url': 'https://telegra.ph/file/bf56e18c0c139db684ea6.jpg'},
     9: {'name': '𝐇𝐚𝐳𝐞𝐥', 'price': 60000000, 'rarity': '🍁 𝐎𝐧𝐢', 'power': 15000, 'img_url': 'https://te.legra.ph/file/2974a9dc120a1239643a2.jpg'},
-    10: {'name': '𝐂𝐨𝐫𝐚𝐥', 'price': 40000000, 'rarity': '🌳 𝐖𝐨𝐫𝐥𝐝 𝐓𝐫𝐞𝐞', 'power': 30000, 'img_url': 'https://te.legra.ph/file/2974a9dc120a1239643a2.jpg'},
+    10: {'name': '𝐂𝐨𝐫𝐚𝐥', 'price': 40000000, 'rarity': '🌳 𝐖𝐨𝐫𝐥𝐝 𝐓𝐫𝐞𝐞', 'power': 30000, 'img_url': 'https://telegra.ph/file/8c62f510ce1f99f951f96.jpg'},
     11: {'name': '𝐁𝐫𝐢𝐚𝐫', 'price': 20000000, 'rarity': '🍂 𝐃𝐚𝐫𝐤 𝐄𝐥𝐟', 'power': 75000, 'img_url': 'https://telegra.ph/file/78d17d6f4cacbaa09459b.jpg'},
     12: {'name': '𝐀𝐮𝐫𝐞𝐥𝐢𝐚', 'price': 80000000, 'rarity': '👹 𝐃𝐞𝐦𝐨𝐧', 'power': 100000, 'img_url': 'https://telegra.ph/file/b72e995608139bf525675.mp4'},
     13: {'name': '𝐀𝐭𝐥𝐚𝐧𝐭𝐚', 'price': 150000000, 'rarity': '🍑 𝐒𝐮𝐜𝐂𝐮𝐜𝐮𝐬', 'power': 150000, 'img_url': 'https://te.legra.ph/file/f32cefc258b5116283eef.png'},
