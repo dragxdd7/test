bfc298b774d6ceb7f2bfc3beedd723f63622d102
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-08-25 16:01:25 +0530

    Update 1v1.py

--- Grabber/modules/1v1.py
+++ Grabber/modules/1v1.py
@@ -1,3 +1,4 @@
+import random
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from . import user_collection, app
