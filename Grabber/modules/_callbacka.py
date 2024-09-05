9c9092b906687a177f917a621eae860ac524fbca
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-09-05 18:21:34 +0530

    Update _callbacka.py

--- Grabber/modules/_callbacka.py
+++ Grabber/modules/_callbacka.py
@@ -3,7 +3,6 @@ from telegram.ext import CallbackContext, CallbackQueryHandler, CommandHandler
 from Grabber import application
 from .cmode import cmode_callback
 from Grabber.utils.button import button_click as bc
-from .delta import sumu
 from .harem import harem_callback as hc
 from .info import check
 from .ptb_store import sales_list_callback
@@ -21,8 +20,6 @@ async def cbq(update: Update, context: CallbackContext):
         await bc(update, context)
     elif data.startswith('cmode'):
         await cmode_callback(update, context)
-    elif data.startswith('correct'):
-        await sumu(update, context)
     elif data.startswith('harem'):
         await hc(update, context)
     elif data.startswith('check_'):
