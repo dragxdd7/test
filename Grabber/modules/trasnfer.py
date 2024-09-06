190be3e337c1ef317544d3dd1f2a0fdec568bbd6
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-09-06 08:01:43 +0530

    Update trasnfer.py

--- Grabber/modules/trasnfer.py
+++ Grabber/modules/trasnfer.py
@@ -1,6 +1,7 @@
+from pyrogram import filters
 from . import db, collection, user_collection, app, dev_filter, capsify
 
-@app.on_message(dev_filter & app.command("transfer"))
+@app.on_message(dev_filter & filters.command("transfer"))
 async def transfer(_, message):
     args = message.text.split()[1:]
     if len(args) != 2:
