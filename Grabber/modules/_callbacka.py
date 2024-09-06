ab78cb8f2dd6f440ac4d2dcdffd0a4fd4b928745
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-09-06 08:22:02 +0530

    Update _callbacka.py

--- Grabber/modules/_callbacka.py
+++ Grabber/modules/_callbacka.py
@@ -6,7 +6,7 @@ from Grabber.utils.button import button_click as bc
 from .harem import harem_callback as hc
 from .info import check
 from .ptb_store import sales_list_callback
-#from .sgift import confirm_gift, cancel_gift
+from .sgift import confirm_gift, cancel_gift
 from .trade import confirm_trade, cancel_trade
 from .rps import rps_button 
 from .start import button
@@ -27,14 +27,14 @@ async def cbq(update: Update, context: CallbackContext):
         await check(update, context)
     elif data.startswith('saleslist:close'):
         await sales_list_callback(update, context)
-    #elif data.startswith('cancel_gift'):
-        #await cancel_gift(update, context)
+    elif data.startswith('cancel_gift'):
+        await cancel_gift(update, context)
     elif data.startswith('confirm_trade'):
         await confirm_trade(update, context)
     elif data.startswith('cancel_trade'):
         await cancel_trade(update, context)
-    #elif data.startswith('confirm_gift'):
-        #await confirm_gift(update, context)
+    elif data.startswith('confirm_gift'):
+        await confirm_gift(update, context)
     elif data in ('rock', 'paper', 'scissors', 'play_again'):
         await rps_button(update, context)
         
