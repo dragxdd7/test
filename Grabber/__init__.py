8a37d36896fb9da158e1da51ff783cbf458da53b
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-09-08 07:59:40 +0530

    Update __init__.py

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -46,7 +46,13 @@ api_id = 20457610
 api_hash = "b7de0dfecd19375d3f84dbedaeb92537"
 
 application = Application.builder().token(TOKEN).build()
-Grabberu = Client("Grabber", api_id, api_hash, bot_token=TOKEN)
+Grabber = Client(
+    "Grabber",
+    api_id=api_id,
+    api_hash=api_hash,
+    bot_token=TOKEN,
+    plugins=dict(root="Grabber/modules")
+)
 app = Grabberu
 client = AsyncIOMotorClient(mongo_url)
 db = client['Character_catcher']
