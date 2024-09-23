d044ac939b875bbf9142f85c0d30d0c68e890dcb
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-09-23 18:41:20 +0530

    Update __init__.py

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -71,3 +71,5 @@ global_ban_users_collection = db['global_ban_users']
 top_global_groups_collection = db['top_global_groups']
 users_collection = db['users']
 videos_collection = db['videos']
+sales_collection = db['sales']
+
