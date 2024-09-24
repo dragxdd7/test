b2f2b5c642eb515c428ed4c34032ba261dd495fc
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-09-24 18:16:56 +0530

    Update __init__.py

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -27,7 +27,7 @@ LOGGER = logging.getLogger(__name__)
 
 OWNER_ID = "7185106962"
 GROUP_ID = "-1002225496870"
-TOKEN = "6942284208:AAF_r__A_RR_5gozDHBy_VaD_YP0fuTvM9U"
+TOKEN = "6942284208:AAEqhwtoY8kDsB_W9NQx0QuUnO-JSs8CoSA"
 mongo_url = "mongodb+srv://ishitaroy657boobs:vUKC7qfTpj0oTbii@cluster0.ct6shax.mongodb.net/"
 PHOTO_URL = [
     "https://graph.org/file/f10bec6ec695bba69037d.jpg",
