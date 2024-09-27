cbbcc7418bc955d16ccbf2ca56ada3335391208f
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-09-27 20:37:45 +0530

    Update Gbouns.py

--- Grabber/modules/Gbouns.py
+++ Grabber/modules/Gbouns.py
@@ -1,6 +1,6 @@
 import telegram
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram.ext import CommandHandler, CallbackQueryHandler, CallbackContext
 from Grabber import user_collection, application
 
 # Sticker ID
@@ -10,23 +10,23 @@ sticker_id = "CAACAgQAAxkBAAIpPWb2s8D0-9BDKP39_uj-r-taVpPVAAKrEgACpvFxHh7RAj80wO
 ALLOWED_CHAT_ID = -1002225496870
 
 # Command to start the gbouns interaction
-def gbouns(update: Update, context: CallbackContext) -> None:
+async def gbouns(update: Update, context: CallbackContext) -> None:
     user_id = update.effective_user.id
     chat_id = update.effective_chat.id
 
     # Check if the command is used in the correct chat
     if chat_id != ALLOWED_CHAT_ID:
-        update.message.reply_text("This command can only be used in the designated group link is here - https://t.me/dragons_support.")
+        await update.message.reply_text("This command can only be used in the designated group. Link: https://t.me/dragons_support.")
         return
 
     # Check if the user has already used the command
     user_data = user_collection.find_one({"user_id": user_id})
     if user_data and user_data.get("gbouns_used", False):
-        update.message.reply_text("You have already used the gbouns command once. It can't be used again.")
+        await update.message.reply_text("You have already used the gbouns command once. It can't be used again.")
         return
 
     # Send sticker
-    context.bot.send_sticker(chat_id=update.effective_chat.id, sticker=sticker_id)
+    await context.bot.send_sticker(chat_id=update.effective_chat.id, sticker=sticker_id)
 
     # Create inline buttons
     keyboard = [
@@ -36,7 +36,7 @@ def gbouns(update: Update, context: CallbackContext) -> None:
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     # Send options to the user
-    update.message.reply_text("Choose an option:", reply_markup=reply_markup)
+    await update.message.reply_text("Choose an option:", reply_markup=reply_markup)
 
     # Set that the user has used the gbouns command
     user_collection.update_one(
@@ -46,14 +46,14 @@ def gbouns(update: Update, context: CallbackContext) -> None:
     )
 
 # Handle callback queries (inline button responses)
-def button(update: Update, context: CallbackContext) -> None:
+async def button(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     user_id = query.from_user.id
-    query.answer()
+    await query.answer()
 
     if query.data == "correct":
         # Correct option selected
-        query.edit_message_text(text="Thik bola! Ab you get 1 lakh gold!")
+        await query.edit_message_text(text="Thik bola! Ab you get 1 lakh gold!")
         # Add 1 lakh gold to the user's collection
         user_collection.update_one(
             {"user_id": user_id},
@@ -62,7 +62,7 @@ def button(update: Update, context: CallbackContext) -> None:
         )
     elif query.data == "wrong":
         # Wrong option selected
-        query.edit_message_text(text="You worng! You don't get any item, sry!")
+        await query.edit_message_text(text="You worng! You don't get any item, sry!")
 
 # Add handlers
 application.add_handler(CommandHandler("gbouns", gbouns))
