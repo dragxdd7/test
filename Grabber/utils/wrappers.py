884496ac4d8b96299d41454bc5691b8d4f21a968
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-10-01 19:28:46 +0530

    Update wrappers.py

--- Grabber/utils/wrappers.py
+++ Grabber/utils/wrappers.py
@@ -1,6 +1,31 @@
-from functools import wraps
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from functools import wraps
+
+sudb = db.sudo
+devb = db.dev
+
+def sudocmd(func):
+    @wraps(func)
+    async def wrapper(client, message: Message):
+        user_id = message.from_user.id
+        sudo_user = await sudb.find_one({"user_id": user_id})
+        if not sudo_user:
+            await message.reply_text("You are not authorized to use this command.")
+            return
+        return await func(client, message)
+    return wrapper
+
+def devcmd(func):
+    @wraps(func)
+    async def wrapper(client, message: Message):
+        user_id = message.from_user.id
+        dev_user = await devb.find_one({"user_id": user_id})
+        if not dev_user:
+            await message.reply_text("You are not authorized to use this command.")
+            return
+        return await func(client, message)
+    return wrapper
 
 def nopvt(func):
     @wraps(func)
