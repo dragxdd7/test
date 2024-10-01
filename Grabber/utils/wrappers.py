effa3a84a71c38ff056ca9d2a23ae5d1b72bae52
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-10-01 19:08:30 +0530

    Update wrappers.py

--- Grabber/utils/wrappers.py
+++ Grabber/utils/wrappers.py
@@ -27,3 +27,37 @@ def devcmd(func):
             return
         return await func(update, context)
     return wrapper
+
+
+def nopvt(func):
+    @wraps(func)
+    async def wrapper(u: Update, c):
+        if u.effective_chat.type == 'private':
+            await u.effective_message.reply_text('This command cannot be used in private messages.')
+            return
+        return await func(u, c)
+    return wrapper
+
+
+async def get_chat_id(update: Update):
+    chat_id = None
+    if update.message:
+        chat_id = update.message.chat_id
+    elif update.callback_query and update.callback_query.message:
+        chat_id = update.callback_query.message.chat_id
+    return chat_id
+
+def limit(func):
+    @wraps(func)
+    async def wrapper(update: Update, context):
+        current_chat_id = await get_chat_id(update)
+
+        allowed_chat_id = -1002225496870
+
+        if current_chat_id != allowed_chat_id:
+            await update.message.reply_text("This command is only works in @dragons_support.")
+            return
+
+        return await func(update, context)
+
+    return wrapper
