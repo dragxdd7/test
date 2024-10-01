5b6b3493bfc3b1a1a40151e1cb781bde2e62995d
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-10-01 19:26:21 +0530

    Update wrappers.py

--- Grabber/utils/wrappers.py
+++ Grabber/utils/wrappers.py
@@ -1,63 +1,29 @@
-from Grabber import db, application
-from telegram.ext import CommandHandler
-from telegram import Update
 from functools import wraps
-
-sudb = db.sudo
-devb = db.dev
-
-def sudocmd(func):
-    @wraps(func)
-    async def wrapper(update: Update, context):
-        user_id = update.effective_user.id
-        sudo_user = await sudb.find_one({"user_id": user_id})
-        if not sudo_user:
-            await update.message.reply_text("You are not authorized to use this command.")
-            return
-        return await func(update, context)
-    return wrapper
-
-def devcmd(func):
-    @wraps(func)
-    async def wrapper(update: Update, context):
-        user_id = update.effective_user.id
-        dev_user = await devb.find_one({"user_id": user_id})
-        if not dev_user:
-            await update.message.reply_text("You are not authorized to use this command.")
-            return
-        return await func(update, context)
-    return wrapper
-
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
 def nopvt(func):
     @wraps(func)
-    async def wrapper(u: Update, c):
-        if u.effective_chat.type == 'private':
-            await u.effective_message.reply_text('This command cannot be used in private messages.')
+    async def wrapper(client, message: Message):
+        if message.chat.type == 'private':
+            await message.reply_text("This command cannot be used in private messages.")
             return
-        return await func(u, c)
+        return await func(client, message)
     return wrapper
 
-
-async def get_chat_id(update: Update):
-    chat_id = None
-    if update.message:
-        chat_id = update.message.chat_id
-    elif update.callback_query and update.callback_query.message:
-        chat_id = update.callback_query.message.chat_id
-    return chat_id
+async def get_chat_id(message: Message):
+    return message.chat.id
 
 def limit(func):
     @wraps(func)
-    async def wrapper(update: Update, context):
-        current_chat_id = await get_chat_id(update)
-
+    async def wrapper(client, message: Message):
+        current_chat_id = message.chat.id
         allowed_chat_id = -1002225496870
 
         if current_chat_id != allowed_chat_id:
-            await update.message.reply_text("This command is only works in @dragons_support.")
+            await message.reply_text("This command only works in @dragons_support.")
             return
 
-        return await func(update, context)
+        return await func(client, message)
 
     return wrapper
