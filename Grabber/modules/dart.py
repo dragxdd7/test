ec21d67659f681e51d745971e40b092185992531
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-11-30 20:33:29 +0530

    Update dart.py

--- Grabber/modules/dart.py
+++ Grabber/modules/dart.py
@@ -7,24 +7,19 @@ from Grabber import user_collection
 from . import add, deduct, show, app
 from .block import block_dec
 
-cooldown_duration_roll = 30  # 30 seconds
-
-# Dictionary to store the last usage time of the roll command for each user
+cooldown_duration_roll = 30
 last_usage_time_roll = {}
 
-# Command to roll the dart
 @app.on_message(filters.command(["dart"]))
 @block_dec
 async def roll_dart(client: Client, message: t.Message):
     user_id = message.from_user.id
     current_time = time.time()
 
-    # Check if the user is registered
     if not await user_collection.find_one({'id': user_id}):
         await message.reply("You need to grab some slave first.")
         return
 
-    # Check if the user has used the command recently
     if user_id in last_usage_time_roll:
         time_elapsed = current_time - last_usage_time_roll[user_id]
         remaining_time = max(0, cooldown_duration_roll - time_elapsed)
@@ -35,49 +30,39 @@ async def roll_dart(client: Client, message: t.Message):
     if len(command_parts) != 2:
         return await message.reply_text("Invalid command.\nUsage: /dart 10000")
 
-    # Get the bet amount from the command
     try:
         dart_amount = int(command_parts[1])
     except ValueError:
         return await message.reply_text("Invalid amount.")
 
-    # Get the user's balance
     bal = await show(user_id)
     if bal is None:
         return await message.reply_text("You don't have enough cash to place this bet.")
 
-    # Check if the user has enough tokens to place the bet
     if dart_amount > bal:
         return await message.reply_text("Insufficient balance to place this bet.")
 
     min_bet_amount = int(bal * 0.07)
-
-    # Check if the amount is greater than or equal to the minimum bet amount
     if dart_amount < min_bet_amount:
         return await message.reply_text(f"Please bet at least 7% of your balance, which is ₩{min_bet_amount}.")
 
-    # Roll the dart
     value = await client.send_dice(chat_id=message.chat.id, emoji="🎯")
-
     await asyncio.sleep(2)
-    if value.dice.value in [4, 5, 6]:
-        await add(user_id, dart_amount)
-        win_message = await message.reply_text(f"[🎯](https://graph.org//file/377806066e2e43256dd00.jpg) You're lucky!\nYou won ₩{dart_amount}")
-        # Add XP for winning
+
+    if value.dice.value == 6:
+        reward_amount = dart_amount * 3
+        await add(user_id, reward_amount)
+        await message.reply_text(f"[🎯](https://graph.org//file/377806066e2e43256dd00.jpg) You're lucky!\nYou won ₩{reward_amount}")
         await add_xp(user_id, 4)
     else:
         await deduct(user_id, dart_amount)
-        lose_message = await message.reply_text(f"[🎯](https://graph.org//file/377806066e2e43256dd00.jpg) Better luck next time!\nYou lost ₩{dart_amount}")
-        # Deduct XP for losing
+        await message.reply_text(f"[🎯](https://graph.org//file/377806066e2e43256dd00.jpg) Better luck next time!\nYou lost ₩{dart_amount}")
         await deduct_xp(user_id, 2)
 
-    # Update the last usage time for the user
     last_usage_time_roll[user_id] = current_time
 
-# Function to add XP to a user
 async def add_xp(user_id, xp_amount):
     await user_collection.update_one({'id': user_id}, {'$inc': {'xp': xp_amount}}, upsert=True)
 
-# Function to deduct XP from a user
 async def deduct_xp(user_id, xp_amount):
     await user_collection.update_one({'id': user_id}, {'$inc': {'xp': -xp_amount}}, upsert=True)
