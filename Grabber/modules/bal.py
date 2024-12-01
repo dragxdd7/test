eaa3bafc4b8afa18fd38bf6dcb7b30cd9849bc6a
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-12-01 16:31:17 +0530

    Update bal.py

--- Grabber/modules/bal.py
+++ Grabber/modules/bal.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from . import Grabberu as app, user_collection, show, sbank, capsify
 from datetime import datetime
-from .block import block_dec
+from .block import block_dec, temp_block
 
 
 @app.on_message(filters.command("bal"))
@@ -13,8 +13,8 @@ async def balance(client: Client, message: Message):
         return
 
     user_id = message.from_user.id
-    #if temp_block(user_id):
-        #return
+    if temp_block(user_id):
+        return
     user_data = await user_collection.find_one({'id': user_id}, projection={'balance': 1, 'saved_amount': 1, 'loan_amount': 1, 'potion_amount': 1, 'potion_expiry': 1})
 
     if user_data:
