995d8c1032f9bd3077d7303deb4d7df9d6564219
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-12-02 19:34:46 +0530

    Update ptb_store.py

--- Grabber/modules/ptb_store.py
+++ Grabber/modules/ptb_store.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as 
 from datetime import datetime as dt
 from . import collection, user_collection, add, deduct, show, app, db, get_image_and_caption, capsify, get_character_ids, get_character, druby
 import random
-from .block import block_dec, block_cbq, temp_block
+from .block import block_dec, temp_block
 
 sdb = db.new_store
 user_db = db.bought
@@ -28,18 +28,13 @@ async def get_user_bought(user_id: int):
     x = await user_db.find_one({"user_id": user_id})
     return x["data"] if x else None
 
-async def get_user_balance(user_id: int):
-    user = await show(user_id)
-    if user:
-        return user.get('balance', 0)
-    return 0
-
 @app.on_message(filters.command("store"))
 @block_dec
 async def shop(client, message):
     user_id = message.from_user.id
     if temp_block(user_id):
         return
+
     x = await get_today_characters(user_id)
 
     if not x or x[0] != today():
@@ -49,12 +44,12 @@ async def shop(client, message):
     else:
         ch_ids = x[1]
 
-    ch_info = [await get_character(cid) for cid in ch_ids]
     photo, caption = await get_image_and_caption(ch_ids[0])
 
     keyboard = [
         [IKB("⬅️", callback_data=f"pg3_{user_id}"), IKB("buy 🔖", callback_data=f"buya_{user_id}"), IKB("➡️", callback_data=f"pg2_{user_id}")],
-        [IKB("refresh 🔄", callback_data=f"refresh_{user_id}"), IKB("close 🗑️", callback_data=f"saleslist:close_{user_id}")]
+        [IKB("refresh 🔄", callback_data=f"refresh_{user_id}")],
+        [IKB("close 🗑️", callback_data=f"saleslist:close_{user_id}")]
     ]
 
     markup = IKM(keyboard)
@@ -62,22 +57,21 @@ async def shop(client, message):
 
 @app.on_callback_query(filters.regex("saleslist:close"))
 async def sales_list_callback(client, query):
-    end_user = int(query.data.split('_')[1])
-    if end_user == query.from_user.id:
-        await query.answer()
-        await query.message.delete()
-    else:
-        await query.answer(capsify('This is not for you baka.'), show_alert=True)
+    user_id = query.from_user.id
+    target_user = int(query.data.split("_")[1])
+    if user_id != target_user:
+        return await query.answer(capsify("This is not for you!"), show_alert=True)
 
-@app.on_callback_query(filters.regex("^buy|^pg|charcnf/|charback/|refresh"))
+    await query.message.delete()
+
+@app.on_callback_query(filters.regex("^buy|^pg|refresh"))
 async def store_callback_handler(client, query):
     data = query.data.split('_')
     origin = int(data[1])
     user_id = query.from_user.id
 
-    user = await user_collection.find_one({'id': user_id})
-    if not user or origin != user_id:
-        return await query.answer(capsify("This is not for you baka."), show_alert=True)
+    if origin != user_id:
+        return await query.answer(capsify("This is not for you!"), show_alert=True)
 
     await query.answer()
 
@@ -85,10 +79,6 @@ async def store_callback_handler(client, query):
         await handle_buy(query, data[0], origin, user_id)
     elif query.data.startswith("pg"):
         await handle_page(query, int(query.data[2]), origin, user_id)
-    elif query.data.startswith("charcnf/"):
-        await handle_char_confirm(query, data[0].split("/")[1], user_id)
-    elif query.data.startswith("charback/"):
-        await handle_char_back(query, data[0].split("/")[1], user_id)
     elif query.data.startswith("refresh"):
         await handle_refresh(query, user_id)
 
@@ -102,7 +92,7 @@ async def handle_buy(query, buy_type, origin, user_id):
         return await query.answer(capsify("You do not have enough coins"), show_alert=True)
 
     await query.edit_message_caption(
-        f"{query.message.caption}\n\n{capsify('__Click on button below to purchase!__')}",
+        f"{query.message.caption}\n\n{capsify('__Click on the button below to purchase!__')}",
         reply_markup=IKM([
             [IKB("purchase 💵", callback_data=f"charcnf/{char}_{user_id}")],
             [IKB(capsify("ʙᴀᴄᴋ 🔙"), callback_data=f"charback/{char}_{user_id}")]
@@ -110,70 +100,21 @@ async def handle_buy(query, buy_type, origin, user_id):
     )
 
 async def handle_page(query, page, origin, user_id):
-    if str(query.message.date).split()[0] != today():
-        return await query.answer(capsify("Query expired, use /store to continue!"), show_alert=True)
-
     y = await get_today_characters(origin)
     char = y[1][page - 1]
     photo, caption = await get_image_and_caption(char)
-    nav_buttons = ["pg1", "pg2", "pg3", 'pg1']
-    buy_buttons = ["buya", "buyb", "buyc", 'buya']
+
+    nav_buttons = ["pg1", "pg2", "pg3", "pg1"]
+    buy_buttons = ["buya", "buyb", "buyc", "buya"]
 
     keyboard = [
         [IKB("⬅️", callback_data=f"{nav_buttons[page-2]}_{user_id}"), IKB("buy 🔖", callback_data=f"{buy_buttons[page-1]}_{user_id}"), IKB("➡️", callback_data=f"{nav_buttons[page]}_{user_id}")],
-        [IKB("refresh 🔄", callback_data=f"refresh_{user_id}"), IKB("close 🗑️", callback_data=f"saleslist:close_{user_id}")]
+        [IKB("refresh 🔄", callback_data=f"refresh_{user_id}")],
+        [IKB("close 🗑️", callback_data=f"saleslist:close_{user_id}")]
     ]
 
     await query.edit_message_media(
         media=IMP(photo, caption=capsify(f"PAGE {page}\n\n{caption}")),
-        reply_markup=IKM(keyboard)
-    )
-
-async def handle_char_confirm(query, char, user_id):
-    det = await get_character(char)
-    price = det.get("price", 0)
-    user_balance = await show(user_id)
-
-    if price > user_balance:
-        return await query.answer(capsify("You do not have enough coins"), show_alert=True)
-
-    bought = await get_user_bought(user_id)
-    if bought and bought[0] == today() and char in bought[1]:
-        return await query.answer(capsify("You've already bought it!"), show_alert=True)
-
-    await query.edit_message_caption(
-        capsify(f"You've successfully purchased {det['name']} for {price} coins."),
-        reply_markup=IKM([[IKB(capsify("back 🔙"), callback_data=f"charback/{char}_{user_id}")]])
-    )
-
-    new_bought = bought[1] if bought and bought[0] == today() else []
-    new_bought.append(char)
-    await update_user_bought(user_id, [today(), new_bought])
-    await deduct(user_id, price)
-
-    await user_collection.update_one(
-        {'id': user_id},
-        {'$addToSet': {'characters': det}},
-        upsert=True
-    )
-
-    await query.answer(capsify("Character bought successfully!"), show_alert=True)
-
-async def handle_char_back(query, char, user_id):
-    y = await get_today_characters(user_id)
-    ch_ids = y[1]
-    ind = ch_ids.index(char) + 1
-    nav_buttons = {1: [3, 2], 2: [1, 3], 3: [2, 1]}
-    buy_buttons = {1: "a", 2: "b", 3: "c"}
-
-    photo, caption = await get_image_and_caption(char)
-    keyboard = [
-        [IKB("⬅️", callback_data=f"pg{nav_buttons[ind][0]}_{user_id}"), IKB("buy 🔖", callback_data=f"buy{buy_buttons[ind]}_{user_id}"), IKB("➡️", callback_data=f"pg{nav_buttons[ind][1]}_{user_id}")],
-        [IKB("refresh 🔄", callback_data=f"refresh_{user_id}"), IKB("close 🗑️", callback_data=f"saleslist:close_{user_id}")]
-    ]
-
-    await query.edit_message_caption(
-        capsify(f"__PAGE {ind}__\n\n{caption}"),
         reply_markup=IKM(keyboard)
     )
 
@@ -192,12 +133,12 @@ async def handle_refresh(query, user_id):
     new_ch_ids = random.sample(ids, 3)
     await set_today_characters(user_id, [today(), new_ch_ids])
 
-    ch_info = [await get_character(cid) for cid in new_ch_ids]
     photo, caption = await get_image_and_caption(new_ch_ids[0])
 
     keyboard = [
         [IKB("⬅️", callback_data=f"pg3_{user_id}"), IKB("buy 🔖", callback_data=f"buya_{user_id}"), IKB("➡️", callback_data=f"pg2_{user_id}")],
-        [IKB("refresh 🔄", callback_data=f"refresh_{user_id}"), IKB("close 🗑️", callback_data=f"saleslist:close_{user_id}")]
+        [IKB("refresh 🔄", callback_data=f"refresh_{user_id}")],
+        [IKB("close 🗑️", callback_data=f"saleslist:close_{user_id}")]
     ]
 
     await query.edit_message_media(
