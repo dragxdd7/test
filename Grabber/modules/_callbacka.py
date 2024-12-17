bb69eecb78396b1c57998df6096cafa05202b5a8
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-12-17 22:34:57 +0530

    Update _callbacka.py

--- Grabber/modules/_callbacka.py
+++ Grabber/modules/_callbacka.py
@@ -5,7 +5,7 @@ from .cmode import cmode_callback
 from Grabber.utils.button import button_click as bc
 #from .harem import harem_callback as hc
 from .info import check
-from .ptb_store import sales_list_callback
+#from .ptb_store import sales_list_callback
 from .sgift import confirm_gift, cancel_gift
 from .trade import confirm_trade, cancel_trade
 from .rps import rps_button 
@@ -20,8 +20,6 @@ async def cbq(update: Update, context: CallbackContext):
         await check(update, context)
     elif data.startswith('cmode'):
         await cmode_callback(update, context)
-    elif data.startswith('saleslist:close'):
-        await sales_list_callback(update, context)
     elif data.startswith('cancel_gift'):
         await cancel_gift(update, context)
     elif data.startswith('confirm_trade'):
