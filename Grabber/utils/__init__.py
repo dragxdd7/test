cdba641c27280c4dd4104e5c59675f61db20b828
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-12-25 09:39:38 +0530

    Update __init__.py

--- Grabber/utils/__init__.py
+++ Grabber/utils/__init__.py
@@ -6,24 +6,3 @@ from .gban import *
 from .disable import *
 from .error import *
 
-
-ALPHABETS = "abcdefghijklmnopqrstuvwxyz"
-ALL_CAPS = "ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ"
-
-def capsify(text: str) -> str:
-    txt = ""
-    for x in text:
-        if x == '\n':
-            txt += '\n'
-        elif x == ' ':
-            txt += ' '
-        else:
-            if x.lower() in ALPHABETS:
-                ind = ALPHABETS.index(x.lower())
-                txt += ALL_CAPS[ind]
-            else:
-                txt += x
-    return txt
-
-async def acapsify(text: str) -> str:
-    return capsify(text)
