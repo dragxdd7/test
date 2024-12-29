7c28504fdaa93a0e1e3c2497ad67b36c4c888f93
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-12-29 17:02:33 +0530

    Update sudo.py

--- Grabber/modules/sudo.py
+++ Grabber/modules/sudo.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, Message 
 from . import app, dev_filter, sudo_filter, capsify, db, user_collection
 
 sudb = db.sudo
@@ -7,6 +7,129 @@ devb = db.dev
 uploaderdb = db.uploader
 
 NEGLECTED_IDS = {6893383681, 7011990425}
+
+@app.on_message(filters.command("addsudo") & dev_filter)
+async def add_sudo(client, message: Message):
+    if message.reply_to_message:
+        tar = message.reply_to_message.from_user.id
+    else:
+        try:
+            tar = int(message.text.split()[1])
+        except Exception:
+            return await message.reply_text(capsify('Either reply to a user or provide an ID.'))
+
+    if tar in NEGLECTED_IDS:
+        return await message.reply_text(capsify('This user cannot be added to the sudo list.'))
+
+    if await sudb.find_one({'user_id': tar}):
+        return await message.reply_text(capsify('User is already a sudo user.'))
+
+    try:
+        await sudb.insert_one({'user_id': tar})
+        await message.reply_text(capsify('User added to sudo list.'))
+    except Exception:
+        await message.reply_text(capsify('Failed to add user to sudo list.'))
+
+@app.on_message(filters.command("rmsudo") & dev_filter)
+async def remove_sudo(client, message: Message):
+    if message.reply_to_message:
+        tar = message.reply_to_message.from_user.id
+    else:
+        try:
+            tar = int(message.text.split()[1])
+        except Exception:
+            return await message.reply_text(capsify('Either reply to a user or provide an ID.'))
+
+    if not await sudb.find_one({'user_id': tar}):
+        return await message.reply_text(capsify('User is not a sudo user.'))
+
+    try:
+        await sudb.delete_one({'user_id': tar})
+        await message.reply_text(capsify('User removed from sudo list.'))
+    except Exception:
+        await message.reply_text(capsify('Failed to remove user from sudo list.'))
+
+@app.on_message(filters.command("adddev") & dev_filter)
+async def add_dev(client, message: Message):
+    if message.reply_to_message:
+        tar = message.reply_to_message.from_user.id
+    else:
+        try:
+            tar = int(message.text.split()[1])
+        except Exception:
+            return await message.reply_text(capsify('Either reply to a user or provide an ID.'))
+
+    if tar in NEGLECTED_IDS:
+        return await message.reply_text(capsify('This user cannot be added to the dev list.'))
+
+    try:
+        await devb.insert_one({'user_id': tar})
+        await message.reply_text(capsify('User added to dev list.'))
+    except Exception:
+        await message.reply_text(capsify('Failed to add user to dev list.'))
+
+@app.on_message(filters.command("rmdev") & dev_filter)
+async def remove_dev(client, message: Message):
+    if message.reply_to_message:
+        tar = message.reply_to_message.from_user.id
+    else:
+        try:
+            tar = int(message.text.split()[1])
+        except Exception:
+            return await message.reply_text(capsify('Either reply to a user or provide an ID.'))
+
+    if tar == 7455169019:
+        return await message.reply_text(capsify('This developer cannot be removed.'))
+
+    if not await devb.find_one({'user_id': tar}):
+        return await message.reply_text(capsify('User is not a developer.'))
+
+    try:
+        await devb.delete_one({'user_id': tar})
+        await message.reply_text(capsify('User removed from dev list.'))
+    except Exception:
+        await message.reply_text(capsify('Failed to remove user from dev list.'))
+
+@app.on_message(filters.command("adduploader") & sudo_filter)
+async def add_uploader(client, message: Message):
+    if message.reply_to_message:
+        tar = message.reply_to_message.from_user.id
+    else:
+        try:
+            tar = int(message.text.split()[1])
+        except Exception:
+            return await message.reply_text(capsify('Either reply to a user or provide an ID.'))
+
+    if tar in NEGLECTED_IDS:
+        return await message.reply_text(capsify('This user cannot be added to the uploader list.'))
+
+    if await uploaderdb.find_one({'user_id': tar}):
+        return await message.reply_text(capsify('User is already an uploader.'))
+
+    try:
+        await uploaderdb.insert_one({'user_id': tar})
+        await message.reply_text(capsify('User added to uploader list.'))
+    except Exception:
+        await message.reply_text(capsify('Failed to add user to uploader list.'))
+
+@app.on_message(filters.command("rmuploader") & sudo_filter)
+async def remove_uploader(client, message: Message):
+    if message.reply_to_message:
+        tar = message.reply_to_message.from_user.id
+    else:
+        try:
+            tar = int(message.text.split()[1])
+        except Exception:
+            return await message.reply_text(capsify('Either reply to a user or provide an ID.'))
+
+    if not await uploaderdb.find_one({'user_id': tar}):
+        return await message.reply_text(capsify('User is not an uploader.'))
+
+    try:
+        await uploaderdb.delete_one({'user_id': tar})
+        await message.reply_text(capsify('User removed from uploader list.'))
+    except Exception:
+        await message.reply_text(capsify('Failed to remove user from uploader list.'))
 
 @app.on_message(filters.command("sudolist") & sudo_filter)
 async def sudo_list(client, message: Message):
