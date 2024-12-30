6b036db2d8e08f9e0726a05904d538fbc3268b4b
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-12-30 09:15:22 +0530

    Update Grabber

---
+++ Grabber/config.py
@@ -0,0 +1,14 @@
+import os
+
+OWNER_ID = "7185106962"
+GROUP_ID = "-1002225496870"
+TOKEN = os.getenv("BOT_TOKEN")
+api_id = 20457610
+api_hash = "b7de0dfecd19375d3f84dbedaeb92537"
+PHOTO_URL = ["https://files.catbox.moe/oai7m9.mp4"]
+SUPPORT_CHAT = "dragona_support"
+UPDATE_CHAT = "PickBotUpdatesHQ"
+BOT_USERNAME = "Okarun_Game_bot"
+CHARA_CHANNEL_ID = -1002235251549
+MONGO_URL = os.getenv("MONGO_URL")
+DATABASE_NAME = "Character_catcher"
