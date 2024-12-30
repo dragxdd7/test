259a33025131049846de6e5e7ab37101abc642a8
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-12-30 09:00:17 +0530

    Update bonus.py

--- Grabber/modules/bonus.py
+++ Grabber/modules/bonus.py
@@ -113,7 +113,7 @@ async def bonus_claim_handler(_, query):
     await query.edit_message_text(caption, reply_markup=markup)
 
 @app.on_callback_query(filters.regex(r"^bo_close_"))
-£@block_cbq
+#@block_cbq
 async def close_bonus_handler(_, query):
     try:
         _, bonus_type, user_id = query.data.split("_")
