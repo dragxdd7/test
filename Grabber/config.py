5ecb06b347d2ffcffe61cd0f54b3327738a644d3
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-12-30 09:16:00 +0530

    Update config.py

--- Grabber/config.py
+++ Grabber/config.py
@@ -10,5 +10,5 @@ SUPPORT_CHAT = "dragona_support"
 UPDATE_CHAT = "PickBotUpdatesHQ"
 BOT_USERNAME = "Okarun_Game_bot"
 CHARA_CHANNEL_ID = -1002235251549
-MONGO_URL = os.getenv("MONGO_URL")
+MONGO_URL = "mongodb+srv://ishitaroy657boobs:vUKC7qfTpj0oTbii@cluster0.ct6shax.mongodb.net/"
 DATABASE_NAME = "Character_catcher"
