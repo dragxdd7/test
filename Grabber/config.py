12b8f522178ca8794a265f8d5d8d5e42068e8123
Pratham <74235427+Geektyper@users.noreply.github.com>
2025-01-12 14:42:15 +0530

    Update config.py

--- Grabber/config.py
+++ Grabber/config.py
@@ -12,3 +12,4 @@ BOT_USERNAME = "Okarun_Game_bot"
 CHARA_CHANNEL_ID = -1002235251549
 MONGO_URL = "mongodb+srv://ishitaroy657boobs:vUKC7qfTpj0oTbii@cluster0.ct6shax.mongodb.net/"
 DATABASE_NAME = "Character_catcher"
+LOG_CHAT_ID = -1002203193964
