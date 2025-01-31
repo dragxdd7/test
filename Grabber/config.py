06d78fbaa9c26d399f4be4a805aac4f5a66d7fe1
dragxdd7 <2313181058316@thenewcollege.edu.in>
2025-01-31 11:36:17 +0530

    Update config.py

--- Grabber/config.py
+++ Grabber/config.py
@@ -6,9 +6,9 @@ TOKEN = os.getenv("TOKEN")
 api_id = 7155513
 api_hash = "aea00e5d96c3eb15647ca25c0f93a234"
 PHOTO_URL = ["https://files.catbox.moe/oai7m9.mp4"]
-SUPPORT_CHAT = ""
-UPDATE_CHAT = ""
-BOT_USERNAME = "testing sabo"
+SUPPORT_CHAT = "waifutest"
+UPDATE_CHAT = "waifutest"
+BOT_USERNAME = "testingsabo"
 CHARA_CHANNEL_ID = -1002309327815
 MONGO_URL = "mongodb+srv://xdragxt:drag123@dragtest.2b4zj.mongodb.net/?retryWrites=true&w=majority&appName=dragtest/"
 DATABASE_NAME = "testdb"
